ae9add3d9c9bbb6b943ac30f5e7999abc8df31cb
sureshkrnd <68851037+sureshkrnd@users.noreply.github.com>
2020-08-05 17:52:47 +0530

    Update uidgen.h
    updated

--- uidgen.h
+++ uidgen.h
@@ -1,5 +1,7 @@
 #include <cstdlib>
-#include <string>
+// #include <string>
+#include <bits/stdc++.h> 
+#include <boost/algorithm/string.hpp> 
 
 std::string uidgen() {
   std::string uid = "1.4.444.90004.223.";
@@ -7,8 +9,14 @@ std::string uidgen() {
   return uid + std::to_string(suffix);
 }
 
-// std::string uid() {
-//   std::string uid = "1.4.444.90004.223.";
-//   unsigned int suffix = std::rand() * std::rand();
-//   return uid + std::to_string(suffix);
-// }
+std::string uid( std::string uid ) {
+  vector<string> result; 
+  boost::split(result, uid, boost::is_any_of(".")); 
+  
+  for (int i = 0; i < result.size(); i++)
+  {
+    if( result[i].length() > 5 )
+      return False;
+  } 
+    return True; 
+}
