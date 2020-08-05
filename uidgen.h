9ba0fd8b80d9f249c0a8695ddf545d0f463ddbe7
sureshkrnd <68851037+sureshkrnd@users.noreply.github.com>
2020-08-05 21:19:22 +0530

    Update uidgen.h
    updated

--- uidgen.h
+++ uidgen.h
@@ -4,7 +4,7 @@
 
 std::string uidgen() {
   std::string uid = "1.4.444.90004.223.";
-  unsigned int suffix = std::rand() * std::rand();
+  unsigned int suffix = std::rand();// * std::rand();
   return uid + std::to_string(suffix);
 }
 
