997c7f9c741a99923656a9d2cdd36ab13a54931b
sureshkrnd <68851037+sureshkrnd@users.noreply.github.com>
2020-08-05 17:31:57 +0530

    Update uid-gen-test.cpp
    updated

--- uid-gen-test.cpp
+++ uid-gen-test.cpp
@@ -5,9 +5,15 @@
 
 TEST(UID, UID_is_generated_within_length_limit) {
   std::string uid = uidgen();
-  std::cout<<uid;
+  std::cout<<uid.length();
   ASSERT_TRUE(uid.length() > 0);
 }
+
+// TEST(UID, UID_is_generated_split_if_more_than_5_digits) {
+//   std::string uid = uidgen();
+//   uid = uid_split(uid);
+//   ASSERT_TRUE(uid.length() > 0);
+// }
 
 
 int main(int argc, char **argv) {
